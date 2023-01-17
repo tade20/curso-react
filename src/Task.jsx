546ca68b063ab78113973cf00da5c81b0f2ed8ef
tade20 <tadeomachin@gmail.com>
2023-01-17 12:59:12 -0300

    Probando estilos

---
+++ src/Task.jsx
@@ -0,0 +1,10 @@
+import './task.css'
+export function TaskCard(ready) {
+    return <div className='card'>
+        <h1>Mi primer tarea</h1>
+        <span 
+        className={ready ? 'bg-ready' : 'bg-waitting'}>
+            {ready ? 'Tarea realizada' : 'Tarea pendiente'}
+        </span>
+    </div>
+}
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Greeting } from './Greeting';
 import Product from './Product';
 import { UserCard } from './UserCard';
 import { Button } from './Button';
+import { TaskCard } from './Task';
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
@@ -24,8 +25,9 @@ root.render(<>
             street: "Luis Alberto de Herrera 2345",
             city: "Montevideo"
         }}
-    greet={function () { alert("hola mundo") }} />*/}
+    greet={function () { alert("hola mundo") }} />
     <Button text="Hello World" />
     <Button text="Pay" />
-    <Button text="Click Me" />
+    <Button text="Click Me" />*/}
+    <TaskCard />
 </>)
